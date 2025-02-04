00e2c083f876342e699ca76a4d89d9f4ed06aa56
rachelmdaniel <rachelmdaniel3@gmail.com>
2025-02-04 04:05:58 +0000

    Add setup for git global credentials

---
+++ setup_git_global_creds.sh
@@ -0,0 +1,11 @@
+!#/usr/bin/bash
+
+USER=rachelmdaniel3@gmail.com
+NAME=rachelmdaniel
+
+git config --global --list
+
+git config --global user.email ${USER} 
+git config --global user.name  ${NAME} 
+
+git config --global –list
