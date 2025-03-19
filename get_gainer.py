e32a96b1196cb8f367a3130d7f70464afe6b73ff
rachelmdaniel <rachelmdaniel3@gmail.com>
2025-03-19 01:00:16 +0000

    Edited code according to linting recommendations.

--- get_gainer.py
+++ get_gainer.py
@@ -1,18 +1,41 @@
+"""
+Module for initiating the download, processing,and saving of gainer files.
+
+"""
+
 class ProcessGainer:
+    """
+    Template Class containing functions that result in a saved and processed gainers file.
+
+    """
     def __init__(self, gainer_downloader, gainer_normalizer):
         self.downloader = gainer_downloader
         self.normalizer = gainer_normalizer
 
     def _download(self):
+        """
+        Function for downloading data.
+        """
         self.downloader.download()
 
     def _normalize(self):
+        """
+        Function for normalizing data.
+        """
+
         self.normalizer.normalize()
 
     def _save_to_file(self):
+        """
+        Function for saving data file with timestamp.
+        """
         self.normalizer.save_with_timestamp()
 
     def process(self):
+        """
+        Function for processing source data.
+        """
+
         self._download()
         self._normalize()
         self._save_to_file()
