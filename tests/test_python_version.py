5ed50f7b8015afc3c9ded99b445b086c31f3095e
rachelmdaniel <rachelmdaniel3@gmail.com>
2025-04-09 01:10:53 +0000

    Updating python types

--- tests/test_python_version.py
+++ tests/test_python_version.py
@@ -1,7 +1,7 @@
 import sys
 
 def test_python_version():
-    assert sys.version_info[:2] in [(3,10),(3,11),(3,12)], "Python version not 3.10 or 3.11"
+    assert sys.version_info[:2] in [(3,12),(3,13)], "Python version not 3.12 or 3.13"
 
 if __name__ == "__main__":
     pytest.main()
